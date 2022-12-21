aeec7f0fd70541d8a29994649e0937a8b7732a87
Guilherme Costa <guilhermecosta.ds1@gmail.com>
2022-12-21 00:06:41 -0300

    Add pieces(King, Rook) and initial board

--- src/main/java/boardgame/Board.java
+++ src/main/java/boardgame/Board.java
@@ -36,4 +36,8 @@ public class Board {
         return  pieces[position.getRow()][position.getCollum()];
     }
 
+    public void placePiece(Piece piece, Position position){
+        pieces[position.getRow()][position.getCollum()] = piece;
+        piece.position = position;
+    }
 }
--- src/main/java/chess/ChessMatch.java
+++ src/main/java/chess/ChessMatch.java
@@ -1,12 +1,17 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Piece;
+import boardgame.Position;
+import chess.pieces.King;
+import chess.pieces.Rook;
 
 public class ChessMatch {
 
     private Board board;
     public ChessMatch(){
         board = new Board(8,8);
+        initialBoard();
     }
 
     public ChessPiece[][] getPieces(){
@@ -18,5 +23,12 @@ public class ChessMatch {
             }
         }
         return mat;
+    }
+
+    private void initialBoard(){
+        board.placePiece(new Rook(board, Color.WHITE), new Position(2,1));
+        board.placePiece(new King(board,Color.BLACK), new Position(0,4));
+        board.placePiece(new King(board,Color.WHITE), new Position(7,4));
+
     }
 }
